592731838a0357b992fdccca72df484a118f4134
Zongyang Li <lizongyang0527@hotmail.com>
2022-05-28 17:13:53 +0800

    fix toDict issue of Literal null

--- esprima/visitor.py
+++ esprima/visitor.py
@@ -282,7 +282,10 @@ class ToDictVisitor(Visitor):
                 v = yield item
                 k = unicode(k)
                 items.append((self.map.get(k, k), v))
-        yield Visited(dict(items))
+        items = dict(items)
+        if "type" in items.keys() and "raw" in items.keys() and items["type"] == 'Literal' and items["raw"] == "null":
+            items["value"] = None
+        yield Visited(items)
 
     def visit_SRE_Pattern(self, obj):
         yield Visited({})
